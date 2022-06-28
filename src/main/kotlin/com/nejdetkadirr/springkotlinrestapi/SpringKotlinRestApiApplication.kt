7eb0e384e317e43a6d17455c3007001a41edaca1
nejdetkadir <nejdetkadir.550@gmail.com>
2022-06-28 16:22:48 +0300

    Initial commit

---
+++ src/main/kotlin/com/nejdetkadirr/springkotlinrestapi/SpringKotlinRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.nejdetkadirr.springkotlinrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinRestApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/nejdetkadirr/springkotlinrestapi/SpringKotlinRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nejdetkadirr.springkotlinrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinRestApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
